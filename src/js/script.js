bc5120415f3d377b7ad5cf99f2c1536a56400497
rifqi011 <rifqibanusafingi11@gmail.com>
2024-09-17 00:47:30 +0700

    Add name and address from url

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,28 @@
 // Open cover
-const btnCover =  document.getElementById("cover-btn")
-const cover = document.getElementById("cover")
+const btnCover = document.getElementById("cover-btn");
+const cover = document.getElementById("cover");
 
 btnCover.addEventListener("click", () => {
-    cover.classList.add("cover-open")
-})
+	cover.classList.add("cover-open");
+});
+
+// Fungsi untuk mendapatkan query parameter dari URL
+function getQueryParam(param) {
+	const urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get(param);
+}
+
+const name = getQueryParam("name");
+const address = getQueryParam("address");
+
+if (name) {
+	document.getElementById("name").textContent = name;
+} else {
+	document.getElementById("name").textContent = "Nama tidak ditemukan di URL.";
+}
+
+if (address) {
+	document.getElementById("address").textContent = address;
+} else {
+	document.getElementById("address").textContent = "Alamat tidak ditemukan di URL.";
+}
