3ea85b73da4a490386ef0ce1b72f1550f48ed64a
rifqi011 <rifqibanusafingi11@gmail.com>
2024-09-17 15:09:09 +0700

    Add home

--- src/js/script.js
+++ src/js/script.js
@@ -16,13 +16,13 @@ const name = getQueryParam("name");
 const address = getQueryParam("address");
 
 if (name) {
-	document.getElementById("name").textContent = name;
+	document.querySelector(".name").textContent = name;
 } else {
-	document.getElementById("name").textContent = "Nama tidak ditemukan di URL.";
+	document.querySelector(".name").textContent = "Nama tidak ditemukan di URL.";
 }
 
 if (address) {
-	document.getElementById("address").textContent = address;
+	document.querySelector(".address").textContent = address;
 } else {
-	document.getElementById("address").textContent = "Alamat tidak ditemukan di URL.";
+	document.querySelector(".address").textContent = "Alamat tidak ditemukan di URL.";
 }
